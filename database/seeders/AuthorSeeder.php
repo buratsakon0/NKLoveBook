10f10800eb9740d280248f9677c9167a13802996
Nutcha <nutcha_kham@cmu.ac.th>
2025-10-26 21:24:42 +0700

    add seeder for database

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -10,16 +10,24 @@ class AuthorSeeder extends Seeder
     public function run(): void
     {
         $authors = [
-            ['AuthorName' => 'คาลิ'], //1
-            ['AuthorName' => 'Saka Mikami'], //2
-            ['AuthorName' => 'ศศิ วีระเศรษฐกุล'], //3
-            ['AuthorName' => 'คาโกะ ฮาเซงาวะ'], //4
+            ['AuthorName' => 'คาลิ'], //1 Fiction
+            ['AuthorName' => 'Saka Mikami'], //2 Comics/Manga
+            ['AuthorName' => 'ศศิ วีระเศรษฐกุล'], //3 Art & Design
+            ['AuthorName' => 'คาโกะ ฮาเซงาวะ'], //4 Children's Book
 
             // Art & Design
             ['AuthorName' => 'Susan Magsamen และ Ivy Ross'], //5
             ['AuthorName' => 'Kanis'], //6
             ['AuthorName' => 'ตะวัน วัตุยา'], //7
             ['AuthorName' => 'กองบรรณาธิการนิตยสาร Room'], //8
+            ['AuthorName' => 'ฮานะ สึกิมิซุ / ซัตสึกิ'], //10
+            ['AuthorName' => 'Wally and Amanda Koval'], //11
+            ['AuthorName' => 'ภาณุ ตรัยเวช'], //12
+            ['AuthorName' => 'มนสิชา รุ่งชวาลนนท์'], //13
+            ['AuthorName' => 'Blake Snyder'], //14
+
+            // Comics/Manga
+            ['AuthorName' => 'Tatsuki Fujimoto'], //9
         ];
 
         foreach ($authors as $author) {
--- database/seeders/PublisherSeeder.php
+++ database/seeders/PublisherSeeder.php
@@ -10,16 +10,24 @@ class PublisherSeeder extends Seeder
     public function run(): void
     {
         $publishers = [
-            ['PublisherName' => 'คาลิ'], //1
-            ['PublisherName' => 'PHOENIX'], //2
-            ['PublisherName' => 'FULLSTOP'], //3
-            ['PublisherName' => 'Amarin Kids'], //4
+            ['PublisherName' => 'คาลิ'], //1 Fiction
+            ['PublisherName' => 'PHOENIX'], //2 Comics/Manga
+            ['PublisherName' => 'FULLSTOP'], //3 Art & Design
+            ['PublisherName' => 'Amarin Kids'], //4 Children's Book
 
             // Art & Design
             ['PublisherName' => 'Bookscape'], //5
             ['PublisherName' => '10 มิลลิเมตร'], //6
             ['PublisherName' => 'เอ็มไอเอส,สนพ./MISBook'], //7
             ['PublisherName' => 'บ้านและสวน'], //8
+            ['PublisherName' => 'Sunday Afternoon'], //10
+            ['PublisherName' => 'BROCCOLI'], //11
+            ['PublisherName' => 'gypzy'], //12
+            ['PublisherName' => 'Salmon Books'], //13
+           // ['PublisherName' => 'Bookscape'], //14
+
+            // Comics/Manga
+            ['PublisherName' => 'Siam Inter Comics'], //9
         ];
 
         foreach ($publishers as $publisher) {
